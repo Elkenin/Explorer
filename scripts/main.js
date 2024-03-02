e72b30a850b718692a200b5b8ffe5d9fd2cfefa6
de4d10ck-23 <johnlloydttortor@gmail.com>
2024-03-03 02:13:26 +0800

    Nasa dm ko po nilagay lahat ng changes kuya frost

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,62 @@
 // CHAT TRIGGERS
-import { world } from '@minecraft/server';
+import { world , system} from '@minecraft/server';
 
 const prefix = "+";
-// function testDimension(player) {
 
-// }
+function cmdScoreboard(player, type, scoreId, score) {
+    player.runCommandAsync(`scoreboard players ${type} @s[tag=!admin] ${scoreId} ${score}`)
+}
+
+system.runInterval(()=>{
+    world.getAllPlayers().forEach(e=>{
+        //const merit = world.scoreboard.getObjective("Merit")?.getScore(e); //Get player Merit
+        const merit = world.scoreboard.getObjective("Merit"); //Get player Merit
+        const gold = world.scoreboard.getObjective("Gold"); //Get player Gold
+        const isShowActionbar = world.scoreboard.getObjective("isShowActionbar");
+        if(merit==undefined) {
+            world.scoreboard.addObjective("Merit", "Merit").setScore(e,0);
+        }
+        if(gold==undefined) {
+            world.scoreboard.addObjective("Gold", "Gold").setScore(e,0);
+        }
+        if(isShowActionbar==undefined) {
+            world.scoreboard.addObjective("isShowActionbar", "isShowActionbar").setScore(e,1);
+        }
+        //console.warn(isShowActionbar?.getScore(e))
+        if(isShowActionbar.getScore(e)==1) {
+            e.runCommandAsync(`titleraw @s[tag=!admin] actionbar {"rawtext":[{"text":"§9Merit:§f ${merit.getScore(e)}"},{"text":"\n§eGold:§f ${gold.getScore(e)}"}]}`); //Actionbar
+        }
+        
+    })
+}, 0); //Interval....20 = 1seconds
+
 
 world.beforeEvents.chatSend.subscribe((data)=>{
     let words = ["b1lat", "b3lat", "belat", "bilat", "bob0", "bobo", "bobp", "bugaret", "bulbol", "d1ckson", "fck u btch", "fcker", "fuck you", "gago", "k1ffy", "k3ffy", "kepay", "kiffy", "kys", "nigga", "p3n1s", "p3nis", "pakshet", "pakyu", "pen1s", "pota", "punyeta", "puta", "putragis", "shet", "stupid fkcer", "stupid shit", "t1t3", "tanga", "tangina mo", "tarantado", "tit1", "tit3", "tite", "ulol", "walang kwenta"];
 
     let found = words.some(word => data.message .toLowerCase().includes(word));
     let chat = data.message;
+    let player = data.sender;
+    //Detect Bad Words
     if (found) {
         data.cancel = true;
-        world.getDimension("overworld").runCommandAsync(`tell ${data.sender.name} No bad words allowed`);
+        player.runCommandAsync(`tellraw @s {"rawtext":[{"text":"§3[System] §4No Bad Words Allowed"}]}`);
+    }
+     //Hide Actionbar
+    else if (chat == prefix+"hidebar") {
+        data.cancel = true;
+        cmdScoreboard(player, "set", "isShowActionbar", 0);
+    }
+    //Show Actionbar
+    else if (chat == prefix+"showbar") {
+        data.cancel = true;
+        cmdScoreboard(player, "set", "isShowActionbar", 1);
     }
     //TELEPORT TO LOBBY
     else if (chat == prefix+"tp lobby") {
         data.cancel = true;
         world.getDimension("overworld").getPlayers().forEach(e=>{
-            if(e.name == data.sender.name) {
+            if(e.name == player.name) {
                 e.runCommandAsync(`tp @s 2 123 -21`);
                 return true;
             }
@@ -29,7 +66,7 @@ world.beforeEvents.chatSend.subscribe((data)=>{
     else if (chat == prefix+"tp nether") {
         data.cancel = true;
         world.getDimension("overworld").getPlayers().forEach(e=>{
-            if(e.name == data.sender.name) {
+            if(e.name == player.name) {
                 e.runCommandAsync(`tp @s[scores={Merit=1500..}] 191 163 -21`);
                 e.runCommandAsync(`say @s[scores={Merit=..1500}] not qualified`);
             }
@@ -40,7 +77,7 @@ world.beforeEvents.chatSend.subscribe((data)=>{
     else if (chat == prefix+"tp guild") {
         data.cancel = true;
         world.getDimension("overworld").getPlayers().forEach(e=>{
-            if(e.name == data.sender.name) {
+            if(e.name == player.name) {
                 e.runCommandAsync(`tp @s[tag=blue] 5580	68 -4239`);
                 e.runCommandAsync(`tp @s[tag=red] 1775	64	-2626`);
                 e.runCommandAsync(`tp @s[tag=white] 6094 66	5782`);
